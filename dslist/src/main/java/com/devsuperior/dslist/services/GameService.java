ceab2c8cdb40574c6a790a0e2543356b427f06ce
Davi <oversouls11@gmail.com>
2025-01-21 18:57:01 -0300

    GET game By Id and Expections

--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDto;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.errors.ResourceNotFoundException;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -15,9 +18,22 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDto> findAll() {
 		List<Game> result = gameRepository.findAll();
+		
+		if (result.isEmpty()) {
+			throw new ResourceNotFoundException("No games found");
+		}
+		
 		return result.stream().map(x -> new GameMinDto(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Game not found for ID: " + id));
+		return new GameDTO(result);
 	}
 
 }
